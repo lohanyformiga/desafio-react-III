3a81565c96e8e388e5275619f92ac4b21fcc3719
lohanyformiga <lohany.formiga@hotmail.com>
2020-11-28 19:03:08 -0300

    atualizando

---
+++ src/Services/dados.js
@@ -0,0 +1,47 @@
+const filmes = [
+    {
+        id: 1,
+        nome: "Mulan",
+        genero: "Animação",
+        capa: "https://vignette.wikia.nocookie.net/mulan/images/7/7f/Mulan_Capa.jpg/revision/latest/scale-to-width-down/340?cb=20120525030942&path-prefix=pt" 
+    },
+    {
+        id: 2,
+        nome: "Star Wars",
+        genero: "Filme",
+        capa: "https://cinemacomrapadura.com.br/imagens/2015/11/20151106-star_wars_episode_vii__the_force_awakens_ver5_xlg.jpg" 
+    },
+    {
+        id: 3,
+        nome: "A Proposta",
+        genero: "comédia romântica",
+        capa: "https://cinema.ptgate.pt/Movies/Photos/6768/_poster001.jpg" 
+    },
+    {
+        id: 4,
+        nome: "Lucifer",
+        genero: "série",
+        capa: "https://i.pinimg.com/originals/76/98/82/76988219e16af25b2c8dd2d9e2ff97c2.jpg" 
+    },
+    {
+        id: 5,
+        nome: "Gotham",
+        genero: "série",
+        capa: "https://br.web.img2.acsta.net/pictures/17/09/21/22/56/0709709.jpg"
+    },
+    {
+        id: 6,
+        nome: "Grey Anatomy",
+        genero: "Série",
+        capa: "https://maryalcantaras.files.wordpress.com/2018/10/greys-anatomy-season-14.jpg?w=640" 
+    },
+    {
+        id: 7,
+        nome: "Law & Order SVU",
+        genero: "Série",
+        capa: "https://3.bp.blogspot.com/-BMvLbCJ41QY/W7ghcwu4YmI/AAAAAAAABQY/VqzjJSqA2GYEtQKJZn1iv8ZI2wwFlecRgCLcBGAs/s1600/S%25C3%25A9rie%2BLaw%2Be%2BOrder%2BSpecial%2BVictims%2BUnit%2Btodas%2Bas%2Btemporadas%2Bdubladas%2Be%2Blegendadas.jpg" 
+    },
+    
+]
+
+export default filmes
